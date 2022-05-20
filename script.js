f6171a22a2f838307cd4875c6ec921e00640df71
Bryan Battenfelder <bryanbatt@yahoo.com>
2022-05-20 12:10:13 +0900

    updated mapbox access token

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoidG9reW9maXhlZCIsImEiOiJja2s3MDU5ZDIwNml6MnZtZmhmOWJxNnBnIn0.eb-Ztn6aihV4Y70fbhqbzQ';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
